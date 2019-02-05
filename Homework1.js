01f1e7b1a6fd9938fa2780d7c26db73280d5b850
Sourav Mansingh <souravsnigdh@gmail.com>
2019-02-05 06:21:05 +0530

    Add files via upload

---
+++ Homework1.js
@@ -0,0 +1,31 @@
+// Details of song from bollywood movie
+var songName = "Kabhi alvida na kehna";
+console.log( songName);
+var movieName = "Kabhi alvida na kehna";
+console.log(movieName);
+
+/* movie name and 
+song name are same */
+var directedBy = "Karan Johar";
+var producedBy = ["Hiroo Yash Johar","Karan Johar"];
+var writternBy = ["Karan Johar","Shibani Bathija", "Niranjan Iyengar"];
+var starring = ["Amitabh Bachchan","Rani Mukherji","Preity Zinta","Shah Rukh Khan","Abhishek Bachchan","Kirron Kher"];
+var runningTime = 192;
+var isBolloywoodMovie = true;
+var movieDetails = { 1: movieName,
+					 2: directedBy,
+					 3: producedBy,
+					 4: writternBy,
+					 5: starring,
+					 6: runningTime,
+					 7: isBolloywoodMovie};
+// printing all the list of variables using console.log
+
+console.log(directedBy);
+console.log(producedBy);
+console.log(writternBy);
+console.log(starring);
+console.log(runningTime);
+console.log(isBolloywoodMovie);
+console.log(movieDetails);
+
